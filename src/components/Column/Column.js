b0ab8e94efa82b66f5faadb1b855a88636bd8fd9
Joanna Jasik <jasik.joanna87@gmail.com>
2019-12-04 23:07:47 +0100

    Add Icon

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,6 +3,9 @@ import styles from './Column.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import {pageContents, listData} from '../../data/dataStore';
+import Icon from '../Icon/Icon.js';
+import Creator from '../Creator/Creator.js';
+import {settings} from '../../data/dataStore';
 
 class Column extends React.Component {
   state = {
@@ -11,6 +14,7 @@ class Column extends React.Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
     cards: PropTypes.array.isRequired,
+    icon: PropTypes.string.isRequired,
   }
   addCard(title){
     this.setState(state => (
@@ -30,7 +34,14 @@ class Column extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.title}</h3>
+        <h3 className={styles.title}>{this.props.title}
+          <span className={styles.icon}>
+            <Icon name={this.props.icon} />
+          </span>
+        </h3>
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+        </div>
       </section>
     )
   }
